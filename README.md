85656e3faa55fcdb8bfdf1eca35caf8d0bc092ae
aryawardanna <aryamadridfc@gmail.com>
2021-02-17 02:00:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## LAPORKAN! - Pengaduan Masyarakat
+
+Ini adalah Source Code Website LAPORKAN! - Layanan Pengaduan Masyarakat yang dibuat menggunakan Framework Laravel 8 dan Tailwindcss.
---
+++ resources/views/includes/admin/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="mt-3 p-3 text-center bg-white text-sm text-gray-600 dark:text-gray-400 dark:bg-gray-800">
+  © 2021 LAPORKAN! | By
+  <a href="#" class="text-blue-500" target="_blank">Arya Wardana</a>
+</footer>
